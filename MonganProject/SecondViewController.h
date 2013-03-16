7b02271c35bdcec862baf2f78b982f008511a100
Matthew Hinkle <matt@zcd.me>
2013-03-16 15:43:02 -0400

    Initial Commit

---
+++ MonganProject/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  MonganProject
+//
+//  Created by Matthew Hinkle on 3/16/13.
+//  Copyright (c) 2013 Matthew Hinkle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
