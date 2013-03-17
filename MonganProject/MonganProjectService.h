dde0b99c7cdf77f6292dd01d07b28abf8db6bd39
mikehershey <mikehershey32@gmail.com>
2013-03-17 19:34:04 -0400

    idk

---
+++ MonganProject/MonganProjectService.h
@@ -0,0 +1,13 @@
+//
+//  MonganProjectService.h
+//  MonganProject
+//
+//  Created by Mike Hershey on 3/17/13.
+//  Copyright (c) 2013 Matthew Hinkle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MonganProjectService : NSObject
+
+@end
---
+++ MonganProject/MonganProjectService.m
@@ -0,0 +1,13 @@
+//
+//  MonganProjectService.m
+//  MonganProject
+//
+//  Created by Mike Hershey on 3/17/13.
+//  Copyright (c) 2013 Matthew Hinkle. All rights reserved.
+//
+
+#import "MonganProjectService.h"
+
+@implementation MonganProjectService
+
+@end
