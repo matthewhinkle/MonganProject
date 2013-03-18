b2dfb01e52c653149f4b7ebc5e5e8f0ee9cfcfca
mikehershey <mikehershey32@gmail.com>
2013-03-17 20:34:33 -0400

    added new class

---
+++ MonganProject/DesiredItemAndProductAreBothHeldInThisClass.m
@@ -0,0 +1,13 @@
+//
+//  DesiredItemAndProductAreBothHeldInThisClass.m
+//  MonganProject
+//
+//  Created by Mike Hershey on 3/17/13.
+//  Copyright (c) 2013 Matthew Hinkle. All rights reserved.
+//
+
+#import "DesiredItemAndProductAreBothHeldInThisClass.h"
+
+@implementation DesiredItemAndProductAreBothHeldInThisClass
+
+@end
