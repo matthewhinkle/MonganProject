7909f8992088eba5ae113acfbd237912e247e529
mikehershey <mikehershey32@gmail.com>
2013-03-17 20:02:57 -0400

    Mike stuff

---
+++ MonganProject/InventoryItemCell.h
@@ -0,0 +1,13 @@
+//
+//  InventoryItemCell.h
+//  MonganProject
+//
+//  Created by Mike Hershey on 3/17/13.
+//  Copyright (c) 2013 Matthew Hinkle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InventoryItemCell : UITableViewCell
+
+@end
---
+++ MonganProject/InventoryItemCell.m
@@ -0,0 +1,29 @@
+//
+//  InventoryItemCell.m
+//  MonganProject
+//
+//  Created by Mike Hershey on 3/17/13.
+//  Copyright (c) 2013 Matthew Hinkle. All rights reserved.
+//
+
+#import "InventoryItemCell.h"
+
+@implementation InventoryItemCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
